4daa002e3906a203da33dc59147017c7fa94578c
KabauPatrick <patrick.kabau@bitz-itc.com>
2025-01-14 14:52:44 -0500

    revised api endpoints for access control

--- RBAC/access_control_system/user_permissions/serializers.py
+++ RBAC/access_control_system/user_permissions/serializers.py
@@ -1,25 +1,30 @@
 from rest_framework import serializers
-from .models import User, Role, Permission, Module
+from .models import Module, Permission, Role, User
 
-class RoleSerializer(serializers.ModelSerializer):
+
+class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Role
+        model = Module
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
-    role = serializers.PrimaryKeyRelatedField(queryset=Role.objects.all())
-    permissions = serializers.PrimaryKeyRelatedField(queryset=Permission.objects.all(), many=True, required=False)  # Add permissions to serializer
-
-    class Meta:
-        model = User
-        fields = '__all__'
 
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permission
         fields = '__all__'
 
-class ModuleSerializer(serializers.ModelSerializer):
+
+class RoleSerializer(serializers.ModelSerializer):
+    permissions = PermissionSerializer(many=True, read_only=True)
+
     class Meta:
-        model = Module
+        model = Role
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(read_only=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
--- RBAC/access_control_system/user_permissions/views.py
+++ RBAC/access_control_system/user_permissions/views.py
@@ -1,96 +1,35 @@
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import IsAuthenticated
-from .models import User, Role, Permission, Module
-from .serializers import UserSerializer, RoleSerializer, PermissionSerializer, ModuleSerializer
-
-def check_permissions(self, permission_name):
-    """
-    Helper function to check if the user has the required permission.
-    """
-    if self.is_superuser:
-        return True  # Superusers bypass all permission checks
-    if self.role and self.role.permissions.filter(name=permission_name).exists():
-        return True
-    return False
-
-class RoleViewSet(viewsets.ModelViewSet):
-    queryset = Role.objects.all()
-    serializer_class = RoleSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        if not self.request.user.is_superuser:
-            raise PermissionDenied("You do not have permission to view roles.")
-        return super().get_queryset()
-
-    def perform_create(self, serializer):
-        check_permissions(self.request.user, "create_role")
-        serializer.save()
-
-    def perform_update(self, serializer):
-        check_permissions(self.request.user, "update_role")
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        check_permissions(self.request.user, "delete_role")
-        instance.delete()
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        if not self.request.user.is_superuser:
-            raise PermissionDenied("You do not have permission to view users.")
-        return super().get_queryset()
-
-    def perform_create(self, serializer):
-        check_permissions(self.request.user, "create_user")  # Permission check remains role-based
-        serializer.save()
-
-    def perform_update(self, serializer):
-        check_permissions(self.request.user, "update_user")
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        check_permissions(self.request.user, "delete_user")
-        instance.delete()
-
-
-class PermissionViewSet(viewsets.ModelViewSet):
-    queryset = Permission.objects.all()
-    serializer_class = PermissionSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        if not self.request.user.is_superuser:
-            raise PermissionDenied("You do not have permission to view permissions.")
-        return super().get_queryset()
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Module, Permission, Role, User
+from .serializers import ModuleSerializer, PermissionSerializer, RoleSerializer, UserSerializer
 
 
 class ModuleViewSet(viewsets.ModelViewSet):
     queryset = Module.objects.all()
     serializer_class = ModuleSerializer
-    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        if self.request.user.is_superuser:
-            return super().get_queryset()
-        if not self.request.user.role or not self.request.user.role.permissions.filter(name="view_module").exists():
-            raise PermissionDenied("You do not have permission to view modules.")
-        return super().get_queryset()
 
-    def perform_create(self, serializer):
-        check_permissions(self.request.user, "create_module")
-        serializer.save()
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
 
-    def perform_update(self, serializer):
-        check_permissions(self.request.user, "update_module")
-        serializer.save()
 
-    def perform_destroy(self, instance):
-        check_permissions(self.request.user, "delete_module")
-        instance.delete()
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, methods=['get'])
+    def permissions_by_module(self, request, pk=None):
+        user = self.get_object()
+        module_id = request.query_params.get('module_id')
+        if module_id:
+            permissions = user.get_permissions_by_module(module_id)
+            serializer = PermissionSerializer(permissions, many=True)
+            return Response(serializer.data)
+        return Response({"error": "Module ID not provided"}, status=400)
