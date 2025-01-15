712367d4e493b92882295cecd032a7b29156a0e6
KabauPatrick <patrick.kabau@bitz-itc.com>
2025-01-14 19:21:43 -0500

    To be reviewed final api's

--- RBAC/access_control_system/user_permissions/serializers.py
+++ RBAC/access_control_system/user_permissions/serializers.py
@@ -15,16 +15,66 @@ class PermissionSerializer(serializers.ModelSerializer):
 
 
 class RoleSerializer(serializers.ModelSerializer):
-    permissions = PermissionSerializer(many=True, read_only=True)
+    permissions = serializers.PrimaryKeyRelatedField(queryset=Permission.objects.all(), many=True)
+    modules = serializers.PrimaryKeyRelatedField(queryset=Module.objects.all(), many=True)
 
     class Meta:
         model = Role
         fields = '__all__'
 
+    def create(self, validated_data):
+        permissions_data = validated_data.pop('permissions', [])
+        modules_data = validated_data.pop('modules', [])
+        role = Role.objects.create(**validated_data)
+
+        # Add permissions and modules to the role (permissions and modules are now IDs, not instances)
+        role.permissions.set(permissions_data)
+        role.modules.set(modules_data)
+
+        return role
+
+    def update(self, instance, validated_data):
+        permissions_data = validated_data.pop('permissions', [])
+        modules_data = validated_data.pop('modules', [])
+
+        # Update role fields
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+
+        # Update permissions
+        instance.permissions.set(permissions_data)
+
+        # Update modules
+        instance.modules.set(modules_data)
+
+        return instance
+
 
 class UserSerializer(serializers.ModelSerializer):
-    role = RoleSerializer(read_only=True)
-
     class Meta:
         model = User
-        fields = '__all__'
+        fields = [
+            'id', 'username', 'first_name', 'last_name', 'email', 'role',
+            'date_joined', 'is_staff', 'is_active', 'is_superuser',
+            'created_at', 'updated_at', 'groups', 'user_permissions'
+        ]
+        read_only_fields = ['id', 'date_joined', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        # Handle password hashing
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        # Handle password updates securely
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
--- RBAC/access_control_system/user_permissions/views.py
+++ RBAC/access_control_system/user_permissions/views.py
@@ -4,15 +4,49 @@ from rest_framework.decorators import action
 from .models import Module, Permission, Role, User
 from .serializers import ModuleSerializer, PermissionSerializer, RoleSerializer, UserSerializer
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, methods=['get'], url_path='permissions_by_module')
+    def permissions_by_module(self, request, pk=None):
+        user = self.get_object()
+        module_id = request.query_params.get('module_id')  # Get the module_id from query parameters
+
+        if not module_id:
+            return Response({"error": "module_id is required."}, status=400)
+
+        try:
+            module = Module.objects.get(id=module_id)  # Fetch the module by ID
+        except Module.DoesNotExist:
+            return Response({"error": "Module not found."}, status=404)
+
+        # Get the user's role and filter permissions through the role's modules
+        if user.role:
+            permissions = Permission.objects.filter(
+                roles=user.role, 
+                roles__modules=module  # Correctly filter permissions through the role's modules
+            ).distinct()
+        else:
+            permissions = Permission.objects.none()  # No permissions if the user has no role
+
+        # Serialize and return the permissions
+        serializer = PermissionSerializer(permissions, many=True)
+        return Response(serializer.data)
 
 class ModuleViewSet(viewsets.ModelViewSet):
     queryset = Module.objects.all()
     serializer_class = ModuleSerializer
 
-
-class PermissionViewSet(viewsets.ModelViewSet):
-    queryset = Permission.objects.all()
-    serializer_class = PermissionSerializer
+    @action(detail=True, methods=['get'], url_path='permissions')
+    def permissions(self, request, pk=None):
+        module = self.get_object()
+        # Filter permissions associated with this module
+        permissions = Permission.objects.filter(modules=module)
+        
+        # Serialize and return permissions
+        serializer = PermissionSerializer(permissions, many=True)
+        return Response(serializer.data)
 
 
 class RoleViewSet(viewsets.ModelViewSet):
@@ -20,16 +54,6 @@ class RoleViewSet(viewsets.ModelViewSet):
     serializer_class = RoleSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    @action(detail=True, methods=['get'])
-    def permissions_by_module(self, request, pk=None):
-        user = self.get_object()
-        module_id = request.query_params.get('module_id')
-        if module_id:
-            permissions = user.get_permissions_by_module(module_id)
-            serializer = PermissionSerializer(permissions, many=True)
-            return Response(serializer.data)
-        return Response({"error": "Module ID not provided"}, status=400)
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
